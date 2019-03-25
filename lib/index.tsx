1bc92aaba4fe6acdc76ce12ccbb9061afe51b9d1
shuaijiezhao <zhaosja@yonyou.com>
2019-03-25 15:39:11 +0800

    first publish success

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Icon from './icon/icon';
-
-const fn: React.MouseEventHandler<Element> = (e) => {
-    console.log(e.target)
-};
-
-ReactDOM.render(<div>
-    <Icon name="wechat"
-          onClick={fn}
-          onMouseEnter={()=>console.log('enter')}
-          onMouseLeave={()=>console.log('leave')}/>
-    {/*<Icon name="qq"/>*/}
-    {/*<Icon name="alipay"/>*/}
-</div>, document.querySelector("#root"));
+export {default as Icon} from './icon/icon'
