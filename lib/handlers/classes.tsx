24c41a02bc65f5e9c63ed585454449c42ec44c6b
shuaijiezhao <zhaosja@yonyou.com>
2019-04-26 15:07:41 +0800

    重构 classes.tsx

--- lib/handlers/classes.tsx
+++ lib/handlers/classes.tsx
@@ -13,19 +13,17 @@ interface ClassToggles {
 }
 
 function scopedClassMaker(prefix: string) {
-    return function (name?: string | ClassToggles, options?: Options) {
-        let result;
-        if (typeof name === 'string' || name === undefined) {
-            result = [prefix, name].filter(Boolean).join('-');
-        } else {
-            result = Object.entries(name).filter(k => k[1])
-                .map(key => key[0])
-                .map(n => [prefix, n].filter(Boolean).join('-')).join(' ');
-        }
+    return function (name: string | ClassToggles, options?: Options) {
+        const namesObj = (typeof name === 'string' || name === undefined) ? {[name]: name} : name;
+        const scoped = Object.entries(namesObj)
+                            .filter(k => k[1] !== false)
+                            .map(key => key[0])
+                            .map(name => [prefix, name].filter(Boolean).join('-'))
+                            .join(' ');
         if (options && options.extra) {
-            return [result, options && options.extra].filter(Boolean).join(' ');
+            return [scoped, options && options.extra].filter(Boolean).join(' ');
         } else {
-            return result;
+            return scoped;
         }
     }
 }
