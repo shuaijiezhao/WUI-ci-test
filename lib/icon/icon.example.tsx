0b14aae01a890820bfbba24f1094465fc9c75b46
shuaijiezhao <zhaosja@yonyou.com>
2019-04-28 15:47:42 +0800

    add icon.example.tsx

---
+++ lib/icon/icon.example.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Icon from "./icon";
+
+export default function () {
+    return (
+        <div>
+            <Icon name="wechat"/>
+            <Icon name="qq"/>
+            <Icon name="alipay"/>
+        </div>
+    )
+}
