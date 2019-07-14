bd761af89b234cf85f2cecc82f964151c03bd0c3
shuaijiezhao <853169204@qq.com>
2019-07-14 21:17:11 +0800

    icon component update

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -1,15 +1,59 @@
 import React from "react";
 import Icon from "./icon";
 
+import "./icon.example.scss";
+
 const IconExample: React.FunctionComponent = () => {
     return (
-        <div className="icon-contain">
-            <Icon name="wechat"/>
-            <Icon name="qq"/>
-            <Icon name="alipay"/>
-            <Icon name="home"/>
-            <Icon name="library"/>
-            <Icon name="cart"/>
+        <div className="wui-panel">
+            <h3>Icon</h3><br/>
+            <p>给 Icon 添加 name 属性就行。</p><br/>
+            <div className="icon-contain">
+                <ul className="clearfix">
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="wechat"/>
+                        </div>
+                        <div>微信</div><br/>
+                        <div>wechat</div>
+                    </li>
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="qq"/>
+                        </div>
+                        <div>qq</div><br/>
+                        <div>qq</div>
+                    </li>
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="alipay"/>
+                        </div>
+                        <div>支付宝</div><br/>
+                        <div>alipay</div>
+                    </li>
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="home"/>
+                        </div>
+                        <div>Home</div><br/>
+                        <div>home</div>
+                    </li>
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="library"/>
+                        </div>
+                        <div>Library</div><br/>
+                        <div>library</div>
+                    </li>
+                    <li>
+                        <div className="icon-outer">
+                            <Icon name="cart"/>
+                        </div>
+                        <div>Cart</div><br/>
+                        <div>cart</div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
