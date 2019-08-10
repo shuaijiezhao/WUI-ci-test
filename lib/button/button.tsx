6054d7fc6cd30b2e6def70d9f797f7ded84f3536
shuaijiezhao <853169204@qq.com>
2019-08-10 20:52:54 +0800

    button component add btnType

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -8,12 +8,13 @@ interface Props {
     bordered?: 'border',
     size?: 'sm' | 'lg' | 'xg',
     shape?: 'block' | 'round' | 'border' | 'squared' | 'floating' | 'pillRight' | 'pillLeft' | 'icon',
-    disabled?: boolean
+    disabled?: boolean,
+    btnType?: 'submit' | 'reset' | 'button'
 }
 
 const Button: React.FunctionComponent<Props> = (props) => {
     return (
-        <button onClick={props.onClick} className={classes('wui-btn',
+        <button type={props.btnType} onClick={props.onClick} className={classes('wui-btn',
             props.colors !== undefined ? `wui-btn-${props.colors}` : undefined,
             props.bordered !== undefined ? `wui-btn-${props.bordered}` : undefined,
             props.size !== undefined ? `wui-btn-${props.size}` : undefined,
@@ -21,6 +22,6 @@ const Button: React.FunctionComponent<Props> = (props) => {
             {props.children}
         </button>
     )
-}
+};
 
 export default Button
