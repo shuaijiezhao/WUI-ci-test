4add0f78187cd184df12c15e3174cad43ad7a4e5
shuaijiezhao <853169204@qq.com>
2021-02-20 09:33:59 +0800

    滚动条样式优化

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { HTMLAttributes } from "react";
+import { HTMLAttributes, UIEventHandler, useState, useEffect, useRef } from "react";
 import scrollBarWidth from "./scroll-width";
 import "./scroll.scss";
 
@@ -7,11 +7,33 @@ interface Props extends HTMLAttributes<HTMLDivElement> {}
 
 const Scroll: React.FC<Props> = (props) => {
     const { children, ...rest } = props;
+    const wrapperRef = useRef<HTMLDivElement>(null);
+    const [barHeight, setBarHeight] = useState(0);
+    const [barTop, setBarTop] = useState(0);
+
+    useEffect(() => {
+        const { current } = wrapperRef;
+        const scrollHeight = current!.scrollHeight;
+        const viewHeight = current!.getBoundingClientRect().height;
+        setBarHeight(viewHeight*viewHeight / scrollHeight);
+    }, [])
+
+    const onScroll: UIEventHandler = (e) => {
+        const { current } = wrapperRef;
+        const scrollHeight = current!.scrollHeight;
+        const viewHeight = current!.getBoundingClientRect().height;
+        const viewTop = current!.scrollTop;
+        setBarTop(viewTop*viewHeight / scrollHeight);
+    }
 
     return (
         <div className={"wui-scroll"} {...rest}>
-            <div className={"wui-scroll-wrapper"} style={{right: -scrollBarWidth()}}>
+            <div ref={wrapperRef} className={"wui-scroll-wrapper"} style={{right: -scrollBarWidth()}}
+                 onScroll={onScroll}>
                 {children}
+            </div>
+            <div className={"wui-scroll-track"}>
+                <div className="wui-scroll-bar" style={{height: barHeight, transform: `translateY(${barTop}px)`}}></div>
             </div>
         </div>
     )
